56deab0614b28f2c279d7f1285ee0b6ca26ff3cc
Rickard Hammar <hammar.rickard@gmail.com>
2021-01-11 21:42:43 +0100

    day 8 part 2

--- day8/index.js
+++ day8/index.js
@@ -16,7 +16,6 @@ let accumulator = 0
 let step = 0
 while (true) {
   const operation = operations[step]
-  console.log(operation)
   if (operations[step].visited) {
     break
   }
@@ -39,3 +38,80 @@ while (true) {
 }
 
 console.log(accumulator)
+
+// part 2
+
+function resetVisited (operations) {
+  for (const operation of operations) {
+    operation.visited = false
+  }
+}
+
+function reachesLast (operations) {
+  const operationsLength = Object.keys(operations).length
+  let accumulator = 0
+  let step = 0
+  while (true) {
+    if (step === operationsLength) {
+      console.log(accumulator)
+      return true
+    }
+    const operation = operations[step]
+    if (operations[step].visited) {
+      return false
+    }
+    operations[step].visited = true
+    switch (operation.operation) {
+      case 'nop':
+        step++
+        break
+      case 'acc':
+        accumulator += operation.value
+        step++
+        break
+      case 'jmp':
+        step += operation.value
+        break
+
+      default:
+        break
+    }
+  }
+}
+
+let position = 0
+for (const operation of operations) {
+  resetVisited(operations)
+  if (operation.operation === 'jmp') {
+    const testOperations = jmpToNop(operations, position)
+    if (reachesLast(testOperations)) {
+      break
+    }
+  }
+  if (operation.operation === 'nop') {
+    const testOperations = nopToJmp(operations, position)
+    if (reachesLast(testOperations)) {
+      break
+    }
+  }
+  position++
+}
+function jmpToNop (initialOperations, position) {
+  const newOperations = { ...initialOperations }
+  newOperations[position] = {
+    operation: 'nop',
+    value: newOperations[position].value,
+    visited: false
+  }
+  return newOperations
+}
+
+function nopToJmp (initialOperations, position) {
+  const newOperations = { ...initialOperations }
+  newOperations[position] = {
+    operation: 'jmp',
+    value: newOperations[position].value,
+    visited: false
+  }
+  return newOperations
+}
